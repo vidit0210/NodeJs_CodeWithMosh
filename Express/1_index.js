9295e87d32ac5245bcbbf30a0098e61beb9868b9
Vidit Shah <vidit0210@gmail.com>
2018-11-04 12:44:40 +0530

    Express2

--- Express/1_index.js
+++ Express/1_index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+const courses =[
+    {id:1,name:'C++'},
+    {id:2,name:'Java'}
+]
 app.get('/',(req,res)=>{
     res.send('Hello Word');
 })
@@ -10,4 +14,16 @@ app.get(courseAPi,(req,res)=>{
     res.send('This is APi Courses');
 })
 
-app.listen(3000,()=>console.log('Listening to port 3000'));
+app.get('/courses/:name',(req,res)=>{
+    const course = courses.find(c => c.name==(req.params.name));
+    if(!course) res.status(404).send("Course Not Found");
+    else res.send(course.name);
+    
+})
+
+app.get('/post/:year/:month',(req,res)=>{
+    res.send(req.params);
+})
+const port=process.env.PORT||3000;
+
+app.listen(port,()=>console.log(`Listening to port ${port}`));
