fae7c83f1fdf38e91f35b16e12b830ac576a00c1
Vidit Shah <vidit0210@gmail.com>
2018-11-04 16:06:51 +0530

    Schema

--- Express/1_index.js
+++ Express/1_index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-
+const Joi = require('joi');
+app.use(express.json());
 const courses =[
     {id:1,name:'C++'},
     {id:2,name:'Java'}
@@ -14,6 +15,9 @@ app.get(courseAPi,(req,res)=>{
     res.send('This is APi Courses');
 })
 
+app.get('/courses',(req,res)=>{
+    res.send(courses);
+})
 app.get('/courses/:name',(req,res)=>{
     const course = courses.find(c => c.name==(req.params.name));
     if(!course) res.status(404).send("Course Not Found");
@@ -21,6 +25,53 @@ app.get('/courses/:name',(req,res)=>{
     
 })
 
+app.post('/courses',(req,res)=>{
+    const course = {
+        id:courses.length + 1,
+        name:req.body.name
+    }
+    courses.push(course);
+    res.send(course);
+    console.log(courses);
+    
+
+})
+
+// app.post('/courses',(req,res)=>{
+//     const schema ={
+//         name:Joi.string().min(3).required()
+//     };
+//     const result = Joi.validate(req.body,schema);
+//     if(result.error){
+//         res.status(400).send(result.error.details[0].message);
+//         return;
+//     }
+//     const course = {
+//         id:courses.length+1,
+//         name:res.body.name
+//     }
+//     courses.push(course);
+//     res.send(course)
+//     console.log(courses);
+    
+// })
+
+app.put('/course/:name',(req,res)=>{
+    const fcourse = courses.find(c=>c.name==req.params.name)
+    if(!fcourse){
+        res.status(404).send("Course Not Found");
+    }
+    //const schema ={name:Joi.string().min(3).required()};
+    //const result = Joi.validate(req.body,schema);
+    //if(result.error){
+      //  res.status(400).send(result.error.details[0].message);
+   // }
+fcourse.name =req.body.name;
+res.send(fcourse);
+console.log(courses);
+})
+
+//Using Joi for Validation
 app.get('/post/:year/:month',(req,res)=>{
     res.send(req.params);
 })
